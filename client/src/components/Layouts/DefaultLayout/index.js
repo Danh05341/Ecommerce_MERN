0558378f00e238714a5bef72422ee0e2ce869b10
Danh05341 <danh05341@gmail.com>
2023-10-25 11:10:28 +0700

    Xây dựng component Home

--- client/src/components/Layouts/DefaultLayout/index.js
+++ client/src/components/Layouts/DefaultLayout/index.js
@@ -2,13 +2,12 @@ import Header from '../../Header'
 import Footer from '../../Footer'
 import Navbar from '../../Navbar'
 
-
 const DefaultLayout = ({ children }) => {
     return (
         <>
             <Header/>
             <Navbar/>
-            <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+            <main className='bg-slate-100 min-h-[calc(100vh)]'>
                 {children}
             </main>
             <Footer />
